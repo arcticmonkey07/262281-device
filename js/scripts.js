e44a6682b0796ef382051d638fc1448e936152b0
Peter <arcticmonkey07@gmail.com>
2018-03-30 00:39:21 +0300

    исправиль главную, доделал страницу каталога

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// модальное окно с картой
 var link = document.querySelector('.contacts-map');
 var popup = document.querySelector('.modal-map');
 var close = document.querySelector('.close');
@@ -11,6 +12,32 @@ close.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.remove('modal-show');
 });
+
+
+// оживление блока услуги
+document.querySelector('.delivery-button')
+  .addEventListener('click', function (evt) {
+    evt.preventDefault();
+    document.querySelector('.services-delivery').classList.remove('dnone');
+    document.querySelector('.services-warranty').classList.add('dnone');
+    document.querySelector('.services-credit').classList.add('dnone');
+  });
+
+document.querySelector('.warranty-button')
+  .addEventListener('click', function (evt) {
+    evt.preventDefault();
+    document.querySelector('.services-warranty').classList.remove('dnone');
+    document.querySelector('.services-delivery').classList.add('dnone');
+    document.querySelector('.services-credit').classList.add('dnone');
+  });
+
+document.querySelector('.credit-button')
+  .addEventListener('click', function (evt) {
+    evt.preventDefault();
+    document.querySelector('.services-credit').classList.remove('dnone');
+    document.querySelector('.services-delivery').classList.add('dnone');
+    document.querySelector('.services-warranty').classList.add('dnone');
+  });
 
 
 
